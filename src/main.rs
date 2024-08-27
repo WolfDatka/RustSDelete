be22086e279279471f3305bdb9726fbf863c58c4
WolfDatka <wolfdatka@gmail.com>
2024-08-27 10:11:15 +0200

    patched funny flaw which made it not work on computers which didn't have sdelete added to path

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::process::Command;
 fn main() {
     let args: Vec<String> = args().collect();
 
-    let exe_path = "sdelete.exe";
+    let exe_path = "C:/RustSDelete/SDelete/sdelete.exe";
     let args = ["-p", "10", "-s", "-nobanner", &args[1]];
 
     Command::new(exe_path)
